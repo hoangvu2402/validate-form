5b681eaa237d4707ccd2ac8d044733c2a3e2b76f
hoangvu2402 <hoang24k69@gmail.com>
2022-07-21 15:57:23 +0700

    first commit

---
+++ action.js
@@ -0,0 +1,103 @@
+function sign_in() {
+    var send = 0
+
+    var name = document.getElementById('name').value 
+    if(name.length == 0) {
+        document.getElementById('name-error').innerHTML = 'Please enter your name'
+    }
+    else {
+        var regex_name = /^[A-ZÀ|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ|È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ|Ì|Í|Ị|Ỉ|Ĩ|Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ|Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ|Ỳ|Ý|Ỵ|Ỷ|Ỹ|Đ][a-zà|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|ì|í|ị|ỉ|ĩ|ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|ỳ|ý|ỵ|ỷ|ỹ]*(?:[ ][A-ZÀ|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ|È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ|Ì|Í|Ị|Ỉ|Ĩ|Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ|Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ|Ỳ|Ý|Ỵ|Ỷ|Ỹ|Đ][a-zà|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|ì|í|ị|ỉ|ĩ|ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|ỳ|ý|ỵ|ỷ|ỹ]*)+$/
+        if(!regex_name.test(name)) {
+            document.getElementById('name-error').innerHTML = 'Please enter a valid name'
+        }
+        else {
+            document.getElementById('name-error').innerHTML = ''
+            send += 1
+        }
+    }
+
+
+    var gender = document.getElementsByName('Gender')
+    var temp = false
+    for(var i of gender) {
+        if(i.checked) {
+            temp = true
+        }
+    }
+    if(!temp) {
+        document.getElementById('gender-error').innerHTML = 'Please choose a gender'
+    }
+    else {
+        document.getElementById('gender-error').innerHTML = ''
+            send += 1
+
+    }
+
+
+    var address = document.getElementById('address').value
+    if(address.length == 0) {
+        document.getElementById('address-error').innerHTML = 'Please enter your address'
+    }
+    else {
+        var address_regex = /^[A-ZÀ|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ|È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ|Ì|Í|Ị|Ỉ|Ĩ|Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ|Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ|Ỳ|Ý|Ỵ|Ỷ|Ỹ|Đ][a-zà|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|ì|í|ị|ỉ|ĩ|ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|ỳ|ý|ỵ|ỷ|ỹ]*?(?:\,?[ ][A-ZÀ|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ|È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ|Ì|Í|Ị|Ỉ|Ĩ|Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ|Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ|Ỳ|Ý|Ỵ|Ỷ|Ỹ|Đ][a-zà|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|ì|í|ị|ỉ|ĩ|ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|ỳ|ý|ỵ|ỷ|ỹ]*)*$/
+        if(!address_regex.test(address)) {
+            document.getElementById('address-error').innerHTML = 'Please enter a valid address'
+        }
+        else {
+            document.getElementById('address-error').innerHTML = ''
+            send += 1
+
+        }
+    }
+
+
+    var email = document.getElementById('email').value
+    if(email.length == 0) {
+        document.getElementById('email-error').innerHTML = 'Please enter your email'
+    }
+    else {
+        var email_regex = /^\w+@\w+\.[a-z]+$/
+        if(!email_regex.test(email)) {
+            document.getElementById('email-error').innerHTML = 'Please enter a valid email'
+        }
+        else {
+            document.getElementById('email-error').innerHTML = ''
+            send += 1
+
+        }
+    }
+
+
+    var password = document.getElementById('password').value
+    if(password.length < 9) {
+        document.getElementById('password-error').innerHTML = 'Your password must contain more than 8 characters'
+    }
+    else {
+        document.getElementById('password-error').innerHTML = ''
+            send += 1
+
+    }
+
+    var re_password = document.getElementById('re-password').value
+    if(re_password != password) {
+        document.getElementById('re-password-error').innerHTML = 'Re-entered password is not the same'
+    }
+    else {
+        document.getElementById('re-password-error').innerHTML = ''
+            send += 1
+
+    }
+
+    if(send != 6) {
+        return false
+    }
+    else {
+        return true
+    }
+
+}
+
+
+function exit() {
+    location.reload()
+}
